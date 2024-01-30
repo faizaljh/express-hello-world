a3821e533ae0f1445e2e44c4719409cfc5da9e87
faizaljh <95737201+faizaljh@users.noreply.github.com>
2024-01-30 17:55:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const app = require('./app')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
+  console.log(`Haloo Fooo http://localhost:${port}`)
   console.log(`Example app listening at http://localhost:${port}`)
 })
